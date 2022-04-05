c9eb6bca48ff7a485565cd1dca5041923c779346
ElianMelo <elianmelomorais@gmail.com>
2022-04-05 14:01:07 -0300

    Formulario e lista utilizando estados

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import Cronometro from '../components/Cronometro';
 import style from './App.module.scss'
+import { Itarefa } from '../types/tarefa';
 
 function App() {
+  const [tarefas, setTarefas] = useState<Itarefa[]>([]);
   return (
     <div className={style.AppStyle}>
-      <Formulario></Formulario>
-      <Lista></Lista>
+      <Formulario setTarefas={setTarefas}></Formulario>
+      <Lista tarefas={tarefas}></Lista> 
       <Cronometro></Cronometro>
     </div>
   );
